1f3ceee7318a5f5d4dc0e822f11e0d00beef51fc
Andrey Axenov <agatissimo@gmail.com>
2019-11-15 12:52:19 +0300

    delete default files, add favicon, icons and images, set colors to vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import colors from 'vuetify/lib/util/colors';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
 	icons: {
     iconfont: 'mdi',
-	}
+	},
+	theme: {
+    themes: {
+      light: {
+        primary: colors.indigo.lighten1,
+        secondary: colors.indigo.lighten5,
+				error: colors.red.accent2,
+				success: colors.teal.accent3,
+				accent: colors.blueGrey.darken3
+      }
+    }
+  }
 });
