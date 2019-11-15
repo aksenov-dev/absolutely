8447ac6e8f4e782cdf07040467b2e874adeed6d4
Andrey Axenov <agatissimo@gmail.com>
2019-11-15 15:53:58 +0300

    create home page and add cards

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 		<v-content class="secondary px-4 pb-4">
 			<!-- renders the matched component for the given path -->
-      <router-view></router-view>
+      <router-view :links="links"></router-view>
     </v-content>
 
 		<!-- footer -->
@@ -31,22 +31,30 @@ export default {
   data: () => ({
     links: [
 			{
-				icon: "mdi-flower-tulip",
-				text: "Природа",
+				icon: "mdi-flower-tulip", 
+				color: 'green lighten-1',
+				textColor: 'green--text text--lighten-1',
+				text: "Природа", 
 				route: "/nature" 
 			},
 			{
 				icon: "mdi-cat",
-				text: "Животные",
+				color: 'indigo lighten-1', 
+				textColor: 'indigo--text indigo--lighten-1',
+				text: "Животные", 
 				route: "/animals"
 			},
 			{
-				icon: "mdi-food-apple",
+				icon: "mdi-food-apple", 
+				color: 'orange lighten-1', 
+				textColor: 'orange--text orange--lighten-1',
 				text: "Еда", 
 				route: "/food"
 			},
 			{
-				icon: "mdi-tshirt-crew",
+				icon: "mdi-tshirt-crew", 
+				color: 'pink lighten-1', 
+				textColor: 'pink--text pink--lighten-1',
 				text: "Одежда", 
 				route: "/clothes"
 			}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,79 @@
 <template>
-  <div class="home">
+	<v-container class="fill-height pb-5" >
+		<v-row class="my-4 mx-auto" style="max-width:854px">
+			<v-col cols="12" sm="6" v-for="(link, i) in links" :key="i">
+				
+				<!-- card -->
+				<v-hover>
+					<template v-slot="{ hover }">
+						<v-card class="mx-auto" width="100%" :elevation="hover ? 12 : 4">
+							
+							<!-- image link -->
+							<router-link :to="link.route" class="td-none">
+								<v-row align="center" style="height:200px" class="mx-0" :class="link.color">
+									<v-col>
+										<v-sheet 
+											color="white" 
+											height="128"
+											width="128"
+											class="mx-auto pt-1 round shadow"
+										>
+											<v-sheet 
+												height="120" 
+												width="120" 
+												class="d-flex round secondary ml-1"
+											>
 
-  </div>
+												<!-- card icon -->
+												<v-icon class="mx-auto" :class="link.textColor" size="108">
+													{{ link.icon }}
+												</v-icon>
+
+											</v-sheet>
+										</v-sheet>
+									</v-col>
+								</v-row>
+							</router-link>
+
+							<!-- text link -->
+							<v-card-actions>
+								<v-btn text :to="link.route" class="primary--text hover-none">
+									{{ link.text }}
+								</v-btn>
+							</v-card-actions>
+
+						</v-card>
+					</template>
+				</v-hover>
+
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 export default {
-  name: 'home'
-}
+	name: 'home',
+	metaInfo: {
+		title: 'Абсолютли'
+	},
+  props: {
+		links: {
+			type: Array,
+			required: true
+		}
+	}
+};
 </script>
+
+<style>
+a.td-none {
+	text-decoration: none;
+}
+.hover-none:hover:before, .hover-none:focus:before {
+	display: none;
+}
+.round {
+	border-radius: 50%;
+}
+</style>
