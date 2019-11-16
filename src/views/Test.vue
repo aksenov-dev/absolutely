1bb4e34ba9824b42c4bddbbeb852f988f732a064
Andrey Axenov <agatissimo@gmail.com>
2019-11-16 17:15:51 +0300

    add progress bar

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -114,6 +114,21 @@
 				/>
 				
 			</v-carousel>
+
+			<!-- progress bar-->
+			<v-row class="px-4">
+					<v-col cols="12" class="py-4 py-sm-5">
+						<v-progress-linear
+							background-color="secondary"
+							color="indigo lighten-4"
+							height="20"
+							class="rounded"
+							:value="`${100 / (frames.length - 1) * currentFrame}`"
+						>
+							<strong class="caption">{{ currentFrame }} / {{ frames.length }}</strong>
+						</v-progress-linear>
+					</v-col>
+				</v-row>
 		</v-card>
 
 	</v-container>
@@ -204,6 +219,9 @@ export default {
 </script>
 
 <style>
+.rounded {
+	border-radius: 28px;
+}
 .round {
 	border-radius: 50%;
 }
